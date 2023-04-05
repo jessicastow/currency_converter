cdd43231d3040d2b9b6e116e7e0be618083150d8
Jessica Sarah Stow <stowjess@gmail.com>
2023-04-05 08:04:27 +0200

    Add files via upload

---
+++ currency_converter_v1.py
@@ -0,0 +1,41 @@
+import requests
+URL = 'http://data.fixer.io/api/latest?access_key=7370ff2962d49abefc56c32f5bc74aa8'
+
+print('This program converts currencies on your date of choice.')
+
+def currency_conversion():
+    from_currency = str(input("Currency you'd like to convert from:")).upper().strip()
+    to_currency   = str(input("Currency you'd like to convert to:")).upper().strip()
+    amount = int(input("Amount you'd like to convert:"))
+
+    date = None
+    today = str(input("Would you like to use today's conversion rate?")).lower().strip()
+
+    if today == 'yes':
+        date = 'latest'
+    else:
+        date = input("Insert date which you'd like to do the conversion for (format: yyyy-mm-dd)")
+        # print(date) # test
+
+    # code below not working
+    # URL = str('http://data.fixer.io/api/' + date + '?access_key=WjcLO0j3I9Nn0aGdiYXyEo1tr6t2LzfC')
+    # print(URL)
+
+    response = requests.get(URL)
+    rate = response.json()['rates'][from_currency]
+    amount_in_EUR = amount/rate
+    amount = amount_in_EUR*(response.json()['rates'][to_currency])
+    amount = round(amount,2)
+    print(amount)
+    again = input('Again?:').lower().strip()
+    if again == 'yes':
+        currency_conversion()
+
+currency_conversion()
+
+# Notes
+
+# Add a 'list_of_currencies' - list of accepted currencies for the API,
+# if inputs don't match this list print error "invalid currency"
+
+# add an option for using historical dates
